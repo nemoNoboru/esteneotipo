a89b3542573ae49df296e0d6841ff7a9c56c62cb
Fvieira <felipevieira@protonmail.com>
2016-01-29 13:11:07 +0100

    subject module finished. It can show, create and delete subjects

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -3,9 +3,15 @@ class SubjectController < ApplicationController
     @subjects = Subject.all
   end
   def create
-    @id = params[:id]
+    name = params[:name]
+    Subject.create :name => name
+    redirect_to "/subject" , notice: "#{name} was successfully created"
   end
   def destroy
-    @id = params[:id]
+    id = params[:id]
+    subject = Subject.find(id)
+    name = subject.name
+    subject.destroy
+    redirect_to "/subject" , notice: "#{name} was successfully deleted"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :subject
+  resources :notes
 end
